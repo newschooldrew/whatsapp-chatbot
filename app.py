2bac4f9e69bd7f1a6e8e1121d587628a34a4e94d
Andrew Perez <andrewperez@Andrews-MacBook-Air.local>
2020-12-18 13:24:31 -0800

    basic set for chatbot

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,request
+from twilio.rest import Client
+import os
+
+app = Flask(__name__)
+
+ACCOUNT_ID = os.environ.get('TWILIO_ACCOUNT')
+TWILIO_TOKEN = os.environ.get('TWILIO_TOKEN')
+
+client = Client(ACCOUNT_ID,TWILIO_TOKEN)
+TWILIO_NUMBER = 'whatsapp:+14155238886'
+def send_msg(msg,recipient):
+    client.messages.create(
+        from_=TWILIO_NUMBER,
+        body=msg,
+        to=recipient
+    )
+
+def process_msg(msg):
+    res = ""
+    if msg == "hi":
+        res = "Welcome to the stock market bot"
+    else:
+        res = "please type hi to get started"
+    return res
+
+@app.route('/webhook',methods=["POST"])
+def webhook():
+    f = request.form
+    msg = f['Body']
+    sender = f['From']
+    res = process_msg(msg)
+    send_msg(res,sender)
+
+    return "OK",200
+
+    app.run(port=5000)
